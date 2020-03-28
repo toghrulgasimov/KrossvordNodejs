f159076d97d1325719d7be8b633a73bbef1cd179
toghrulgasimov <toghrulgasimov@gmail.com>
2020-03-28 10:20:07 +0100

    update

--- KrossvordServer/firebasedelete.js
+++ KrossvordServer/firebasedelete.js
@@ -29,7 +29,7 @@ async function f() {
             let icon = o.icon;
             let name = o.name;
             try {
-                let path = "./icons/"+name;
+                let path = "./icons/"+"wp";
                 if (!fs.existsSync(path)) {
                     await fs.writeFileSync(path, icon);
                 }
