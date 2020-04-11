cef3167932eb1219ef5b56c58fc8e8636233115d
toghrulgasimov <toghrulgasimov@gmail.com>
2020-04-11 18:29:51 +0200

    separete angularjs

---
+++ KrossvordServer/FamilyProtector/html/js/stringUtil.js
@@ -0,0 +1,16 @@
+let stringUtil = {
+    toTime: function(d) {
+        d = parseInt(d);
+        console.log("TOTIME cagrildi --------------");
+        let ans = new Date(d).toLocaleString();
+        ans = ans.substring(0,ans.length-3)
+        return ans;
+    },
+    isToday: function(someDate){
+        console.log("is time cagrildi");
+        const today = new Date();
+        return someDate.getDate() == today.getDate() &&
+            someDate.getMonth() == today.getMonth() &&
+            someDate.getFullYear() == today.getFullYear()
+    }
+}
