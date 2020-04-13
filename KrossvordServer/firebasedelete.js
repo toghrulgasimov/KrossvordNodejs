6381cbb9910c9f9cfcca06a3df057bd4a61d9b7f
toghrulgasimov <toghrulgasimov@gmail.com>
2020-04-13 11:56:49 +0200

    update

--- KrossvordServer/firebasedelete.js
+++ KrossvordServer/firebasedelete.js
@@ -344,27 +344,9 @@ async function f() {
             }
         });
 
-        app.post('/webhook', (request, response) => {
-            const sig = request.headers['stripe-signature'];
-
-            let event;
-
-            try {
-                event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
-            } catch (err) {
-                return response.status(400).send(`Webhook Error: ${err.message}`);
-            }
-
-            // Handle the checkout.session.completed event
-            if (event.type === 'checkout.session.completed') {
-                const session = event.data.object;
-
-                // Fulfill the purchase...
-                handleCheckoutSession(session);
-            }
-
-            // Return a response to acknowledge receipt of the event
-            response.json({received: true});
+        app.post("/checkout", (req, res) => {
+            console.log(req.body);
+            res.send("1");
         });
 
 
