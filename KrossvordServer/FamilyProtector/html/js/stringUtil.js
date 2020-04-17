21a545332f51a5ab5a69b0e433679bec2120b0cb
toghrulgasimov <toghrulgasimov@gmail.com>
2020-04-17 22:37:56 +0200

    update

--- KrossvordServer/FamilyProtector/html/js/stringUtil.js
+++ KrossvordServer/FamilyProtector/html/js/stringUtil.js
@@ -4,9 +4,10 @@ let stringUtil = {
         //console.log("TOTIME cagrildi --------------");
         let ans = new Date(d).toLocaleString();
 
-        ans = ans.substring(0,ans.length-3);
+        let t = ans.substring(0,ans.length-3);
+        ans = t;
         if(ans.split(":").length == 2) {
-            ans = ans.substring(0, ans.length-3);
+            //ans = ans.substring(0, ans.length-3);
         }
         return ans;
     },
