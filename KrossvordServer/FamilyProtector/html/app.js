c601b978d0ad0aba8cd92dadc43b8baa31405c0d
toghrulgasimov <toghrulgasimov@gmail.com>
2020-04-21 15:56:38 +0200

    update

--- KrossvordServer/FamilyProtector/html/app.js
+++ KrossvordServer/FamilyProtector/html/app.js
@@ -83,10 +83,13 @@ let app = angular.module("app", ['stringUtil', 'ui.scroll']).controller("myCtrl"
         return parseInt(ans);
     }
     $scope.cixish = function() {
-        var cookies = $.cookie();
+        var cookies = document.cookie.split(";");
         console.log(cookies);
-        for(var cookie in cookies) {
-            $.removeCookie(cookie);
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i];
+            var eqPos = cookie.indexOf("=");
+            var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+            document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
         }
     }
 
