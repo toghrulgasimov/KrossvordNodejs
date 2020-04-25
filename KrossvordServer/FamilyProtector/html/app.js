fe97cdc4bc3d7a048caac890a9927651d6c27d1c
toghrulgasimov <toghrulgasimov@gmail.com>
2020-04-26 01:41:14 +0200

    d

--- KrossvordServer/FamilyProtector/html/app.js
+++ KrossvordServer/FamilyProtector/html/app.js
@@ -67,6 +67,7 @@ let app = angular.module("app", ['stringUtil', 'ui.scroll']).controller("myCtrl"
     $scope.gunuAzalt = function() {
         $scope.curDay.setDate($scope.curDay.getDate()-1);
         console.log("Gunuazalt basildi");
+        console.log($scope.curDay);
         $scope.changeData();
     }
     $scope.gunuArtir = function() {
