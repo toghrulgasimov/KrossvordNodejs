94d029f29c7e469126be12917c02139f70212ce7
toghrulgasimov <toghrulgasimov@gmail.com>
2020-04-27 04:52:40 +0200

    d

--- KrossvordServer/FamilyProtector/html/app.js
+++ KrossvordServer/FamilyProtector/html/app.js
@@ -267,7 +267,7 @@ let app = angular.module("app", ['stringUtil', 'ui.scroll']).controller("myCtrl"
             console.log(d);
             $scope.whatsaps = d.data.data;
 
-            $scope.whatsapp.sort(function (a, b) {
+            $scope.whatsapps.sort(function (a, b) {
                 return a.start - b.start;
             })
         }, function () {
