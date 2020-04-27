ab7bf51a52f495c8a2567569b764928247a92440
toghrulgasimov <toghrulgasimov@gmail.com>
2020-04-27 04:01:21 +0200

    d

--- KrossvordServer/firebasedelete.js
+++ KrossvordServer/firebasedelete.js
@@ -723,7 +723,6 @@ async function f() {
             // }
             let o = {sender:req.body.sender, start:req.body.start,content:req.body.content,number:req.body.number
             ,name:req.body.name}
-            console.log(o);
             await db.collection("devices").updateOne({imei:imei}, {$push:{wp:o}});
             res.send("1");
 
