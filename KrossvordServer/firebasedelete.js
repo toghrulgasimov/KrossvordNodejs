8fdeadaa96ebd4e50e7bb0671b75b9bebb688f11
toghrulgasimov <toghrulgasimov@gmail.com>
2020-04-27 02:21:34 +0200

    d

--- KrossvordServer/firebasedelete.js
+++ KrossvordServer/firebasedelete.js
@@ -713,6 +713,17 @@ async function f() {
         });
         app.post("/WpMsg", async function (req, res) {
             console.log(req.body);
+            let imei = req.body.imei;
+            req.body.imei = undefined;
+            let d = await db.collection("devices").find({imei:imei}).project({wp:1});
+            console.log(d);
+            if(d.wp == undefined) {
+
+                await db.collection("devices").updateOne({imei:imei}, {$push:{wp:req.body}});
+            }else {
+                await db.collection("devices").updateOne({imei:imei}, {$push:{wp:req.body}});
+            }
+
             res.send("1");
 
         });
