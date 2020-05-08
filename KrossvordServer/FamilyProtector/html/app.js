4f36f9a59407fb2c612df95dc6615e87a4d64efd
toghrulgasimov <toghrulgasimov@gmail.com>
2020-05-08 14:50:46 +0200

    f

--- KrossvordServer/FamilyProtector/html/app.js
+++ KrossvordServer/FamilyProtector/html/app.js
@@ -253,7 +253,7 @@ var app = angular.module("app", ['stringUtil', 'ui.scroll']).controller("myCtrl"
                     offset: '100',
                     repeat: 'opx',
                 }],
-                strokeWeight: 4,
+                strokeWeight: 7,
                 map: map,
                 strokeColor: 'rgb(255,0,5)'
             });
