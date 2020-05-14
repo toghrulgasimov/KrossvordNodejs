d01b97dcd73a85c19f511124f59e4769fc422e84
toghrulgasimov <toghrulgasimov@gmail.com>
2020-05-14 20:42:05 +0200

    fg

--- KrossvordServer/FamilyProtector/html/js/stringUtil.js
+++ KrossvordServer/FamilyProtector/html/js/stringUtil.js
@@ -2,9 +2,9 @@ var stringUtil = {
     toTime: function(d) {
         d = parseInt(d);
         //console.log("TOTIME cagrildi --------------");
-        let ans = new Date(d).toLocaleString();
+        var ans = new Date(d).toLocaleString();
 
-        let t = ans.substring(0,ans.length-3);
+        var t = ans.substring(0,ans.length-3);
         ans = t;
         if(ans.split(":").length == 3) {
             ans = ans.substring(0, ans.length-3);
@@ -13,7 +13,7 @@ var stringUtil = {
     },
     isToday: function(someDate){
         //console.log("is time cagrildi");
-        const today = new Date();
+        var today = new Date();
         return someDate.getDate() == today.getDate() &&
             someDate.getMonth() == today.getMonth() &&
             someDate.getFullYear() == today.getFullYear()
