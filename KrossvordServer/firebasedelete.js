dc9c2f7863c606ab2db3edd4663b78f78a07f6bf
toghrulgasimov <toghrulgasimov@gmail.com>
2020-05-19 12:51:58 +0200

    ff

--- KrossvordServer/firebasedelete.js
+++ KrossvordServer/firebasedelete.js
@@ -1060,11 +1060,14 @@ async function f() {
             console.log(req.cookies);
             let d;
             if(email == "toghrulgasimov@gmail.com") {
-                d = await db.collection("devices").find({name: { $exists: true }}).project({imei:1, name:1});
+                d = await db.collection("devices").find({name: { $exists: true }}).project({imei:1});
             }else {
                 d = await db.collection("devices").find({email:email, parent:"0",name: { $exists: true }}).project({imei:1, name:1});
             }
             let ans = await d.toArray();
+            for(let i = 0; i < ans.length; i++) {
+                ans[i].name = ans[i].name + ans[i].imei;
+            }
             let data = {data:ans};
             console.log(ans);
             res.send(data);
