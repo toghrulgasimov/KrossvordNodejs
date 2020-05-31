41b774b24d8b299ce9aac17cb6ca3bc44adab49a
toghrulgasimov <toghrulgasimov@gmail.com>
2020-05-31 06:33:17 +0200

    dd

--- KrossvordServer/FamilyProtector/html/app.js
+++ KrossvordServer/FamilyProtector/html/app.js
@@ -442,9 +442,9 @@ var app = angular.module("app", ['stringUtil', 'ui.scroll']).controller("myCtrl"
             }
             var ans = [];
             for(var i = 0; i < $scope.gunluk.length; i++) {
-                // if( $scope.gunluk[i].package == 'com.android.systemui') {
-                //     continue;
-                // }
+                if(m == 1 && $scope.gunluk[i].package == 'com.android.systemui') {
+                    continue;
+                }
 
                 //if(t[$scope.gunluk[i].package] == undefined)continue;
                 $scope.gunluk[i].start = parseInt($scope.gunluk[i].start);
