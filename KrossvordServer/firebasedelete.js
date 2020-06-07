ea1858452a5784c86a3a396039228d074247ab10
toghrulgasimov <toghrulgasimov@gmail.com>
2020-06-07 17:53:54 +0200

    dd

--- KrossvordServer/firebasedelete.js
+++ KrossvordServer/firebasedelete.js
@@ -11,7 +11,7 @@ async function f() {
     const DOMAIN = "mg.lookin24.com";
     const mg = mailgun({apiKey: "6d3b67e2434192b5277e46b39e601277-f135b0f1-ae549850", domain: DOMAIN});
     const data = {
-        from: "Turab Qasimov <turabqasimov@gmail.com>",
+        from: "Ilham Aliyev <ilham@prezident.az>",
         to: "farid.naghizada@mail.ru", //farid.naghizada@gmail.com
         subject: "Parolu Deyish",
         text: "Parolu deyishmek ucun link budur"
