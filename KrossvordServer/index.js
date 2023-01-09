e622e11ec1fd7d51b65daafc23c60ca116750c4e
toghrulgasimov@gmail.com <toghrulgasimov@gmail.com>
2023-01-09 15:36:16 +0400

    change yarish time

--- KrossvordServer/index.js
+++ KrossvordServer/index.js
@@ -964,8 +964,8 @@ async function f() {
 			res.send(s);
 		}else {
 			let s =//' Hər hansı bir problemlə üzləkdikdə\n'+
-				' Növbəti Yarış 2023 3 Yanvar(20:00	)\n'+
-				' Yarış 2023 3 Yanvar(21:00) bitir\n'+
+				' Növbəti Yarış 2023 17 Yanvar(20:00	)\n'+
+				' Yarış 2023 17 Yanvar(21:00) bitir\n'+
 				' Nəticələr 2023 3 Yanvar(21:05) elan edilir,\n'+
 				' 1-ci, 2-ci Yerə Ağıllı saatlar hədiyyə.\n'+
 				' 		Saatların funksiyası\n'+
